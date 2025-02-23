b141b94a84e06b29944a7757cce1782603508d70
Junfa Li <1170640485fg@gmail.com>
2025-02-23 18:12:56 -0400

    1. Fixed the 'import react' problem of eslint 2.Finished the basic Button logic Result: Good

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-
+import Button, { ButtonType, ButtonProps, ButtonSize } from "./components/Button/button"
 function App() {
-
   return (
     <>
       <h1>Hello World</h1>
       <h2>Hello World</h2>
       <h3>Hello World</h3>
 
+      <Button> Hello </Button>
+      <br />
+      <Button btnType={"primary"} size={"large"}></Button>
     </>
   )
 }
